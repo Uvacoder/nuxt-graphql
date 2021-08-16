48e70ddaf7bf7a4882604b9aa4a5a7252c1ad01e
amitbiswaswebdev <amit.biswas.webdev@gmail.com>
2021-08-16 11:00:04 +0530

    first commit

---
+++ components/Common/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-img :src="src" :lazy-src="lazySrc">
+    <template v-slot:placeholder>
+      <v-row class="fill-height ma-0" align="center" justify="center">
+        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+      </v-row>
+    </template>
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  data () {
+    return {
+      src : 'https://via.placeholder.com/1920x760.png/09f/fff',
+      lazySrc : 'https://picsum.photos/id/11/100/60'
+    }
+  }
+}
+</script>
---
+++ components/Common/ForgetPassword.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Forgot Password</div>
+</template>
+
+<script>
+export default {
+  name: "ForgetPassword"
+}
+</script>
+
+<style scoped>
+
+</style>
